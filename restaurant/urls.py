1bb5a6e0e92e890628f1c271adb8bb058c15fd7a
Dilmurod <dilmurod@localhost.localdomain>
2023-12-28 00:09:47 +0500

    REST API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,14 @@
 #define URL route for index() view
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework import routers
+from .views import index, MenuItemsView, SingleMenuItemView, BookingViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', index, name='index'),
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
