623ca57ef2aa218020354e61efddb6cd762bbf0b
Dilmurod <dilmurod@localhost.localdomain>
2023-12-27 23:40:19 +0500

    mariadb and models

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Test',
+        'USER': 'admindjango',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'CONN_MAX_AGE': 300,
     }
 }
 
